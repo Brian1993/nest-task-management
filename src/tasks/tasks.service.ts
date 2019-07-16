70aee1a1e282d2841143e33a4a8cf75aa9cad68a
Brian <wl00387719@gmail.com>
2019-07-16 23:20:17 +0800

    remove unused code

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -24,14 +24,6 @@ export class TasksService {
     }
     return found
   }
-  // getTaskById(id: string): Task {
-  //   const found = this.tasks.find(task => task.id === id)
-
-  //   if (!found) {
-  //     throw new NotFoundException(`Task with ID ${id} not found`)
-  //   }
-  //   return found
-  // }
 
   createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskRepository.creaTask(createTaskDto)
