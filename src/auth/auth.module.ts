7698ec3b6111b156507d0de037c69fae8a08aa87
Brian Chen ( 陳弘毅 ) <wl00387719@gmail.com>
2019-07-17 14:09:59 +0800

    add passport.js to validate jwt when user trying access protected route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,33 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserRepository } from './user.repository'
 import KEYS from '../config/keys'
+import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
     JwtModule.register({
-      secret: KEYS.JWT_SECRET
+      secret: KEYS.JWT_SECRET,
+      signOptions: {
+        expiresIn: 3600
+      }
     }),
     TypeOrmModule.forFeature([ UserRepository ])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule
+  ]
 })
 export class AuthModule {}
